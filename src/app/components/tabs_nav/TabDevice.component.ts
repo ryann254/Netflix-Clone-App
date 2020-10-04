80c55c439e99ac0ccd9052a85839609a6921cdf3
Ryan Njoroge <keddelyronjoz@gmail.com>
2020-10-04 13:57:26 +0300

    Added webp images

--- src/app/components/tabs_nav/TabDevice.component.ts
+++ src/app/components/tabs_nav/TabDevice.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "Tab-Device",
   templateUrl: "TabDevice.component.html",
-  styleUrls: ["TabDevice.component.css"]
+  styleUrls: ["TabDevice.component.css"],
 })
 export class TabDeviceComponent {}
